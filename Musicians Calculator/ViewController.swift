159f30e9d45f0d10be7085b90684ab58b75ea7b5
mike <mtaleksa1@gmail.com>
2018-12-14 19:59:50 -0500

    Initial Commit

---
+++ Musicians Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Musicians Calculator
+//
+//  Created by Michael Aleksa on 12/14/18.
+//  Copyright © 2018 Michael Aleksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
