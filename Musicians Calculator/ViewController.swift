6cc57151d59494a8e258159468f72bfdab22d970
mike <mtaleksa1@gmail.com>
2018-12-19 01:10:50 -0500

    Adjust ViewController to limit BPM field input

--- Musicians Calculator/ViewController.swift
+++ Musicians Calculator/ViewController.swift
@@ -8,32 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     var divisions = [Division]()
+    var showMilliseconds = true
+    
+    @IBOutlet weak var durationDisplay: UILabel!
+    @IBOutlet weak var bpmField: UITextField!
 
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bpmField.delegate = self
+        bpmField.keyboardType = .decimalPad
+        
         for name in ["1/64", "1/32", "1/16", "1/8", "1/4", "1/2", "1/1", "2/1"] {
             let division = Division(withName: name)
             divisions.append(division)
         }
     }
     
-    @IBOutlet weak var durationDisplay: UILabel!
-
-    @IBOutlet weak var bpmField: UITextField!
+    // limit BPM field input to numbers and a decimal point
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let invalidCharacters = CharacterSet(charactersIn: "0123456789.").inverted
+        return string.rangeOfCharacter(from: invalidCharacters) == nil
+    }
     
+    // when BPM field is updated, update the BPM in the model and reload the Duration Display
     @IBAction func editBPM(_ sender: UITextField) {
         let doubleBPM = Double(sender.text!) ?? 0
         Division.setBPM(withbpm: doubleBPM)
         updateDuration(withbpm: doubleBPM, on: durationDisplay)
     }
 
+    // reload the Duration Display
     func updateDuration(withbpm bpm: Double, on label: UILabel) {
         durationDisplay.numberOfLines = 0
         var newLabel : String = ""
         for division in divisions {
-            newLabel += division.getName() + " note:\t" + String(format: "%.2f", division.getMilliseconds()) + " ms\n"
+            if (showMilliseconds) {
+                newLabel += division.getName() + " note:\t" + String(format: "%.2f", division.getMilliseconds()) + " ms\n"
+            }
+            else {
+                newLabel += division.getName() + " note:\t" + String(format: "%.0f", division.getSamples()) + " ms\n"
+            }
         }
         label.text = newLabel
     }
